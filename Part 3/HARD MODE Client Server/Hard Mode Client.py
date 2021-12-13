da06e531ceb282659616db6dcc0e81a370d8ffe6
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2021-12-13 12:18:56 +0530

    Add files via upload

---
+++ Part 3/HARD MODE Client Server/Hard Mode Client.py
@@ -0,0 +1,125 @@
+from queue import Queue
+
+from socket import AF_INET
+from socket import error
+from socket import SOCK_STREAM
+from socket import socket
+from socket import SOL_SOCKET
+from socket import SO_REUSEADDR
+
+from signal import SIGINT
+from signal import signal
+from signal import SIGTERM
+
+from sys import exit
+
+from threading import Thread
+
+from time import sleep
+
+host = 'localhost'
+port = 8888
+sock = None
+
+queue = Queue()
+temp = [1,2]
+
+commands = [
+    "connections: Gives all the connections with server",
+    "send <id> <message>: To send <message> to client <id>",
+    "close: To close the connection with server"
+]
+
+def create_socket():
+    global sock
+    try:
+        sock = socket(AF_INET, SOCK_STREAM)
+    except error as msg:
+        print("Socket creation error: " + str(msg))
+    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+def connect():
+    try:
+        sock.connect((host, port))
+    except error as msg:
+        print("Socket Connection error: " + str(msg))
+
+def quit_gracefully(signal = None, frame = None):
+    if sock:
+        try:
+            sock.shutdown(2)
+            sock.close()
+        except Exception as msg:
+            print('Could not close connection:' + str(msg))
+    exit()
+
+def receive_work():
+    receive_from_server()
+    queue.task_done()
+
+def send_work():
+    send_to_server()
+    queue.task_done()
+
+def create_jobs():
+    for x in temp:
+        queue.put(x)
+    queue.join()
+    return
+
+def send_to_server():
+    print("Enter any of the commands given below")
+    for x in commands:
+        print(x)
+    print(">>>>>", end = "")
+    while True:
+        command_str = input()
+        if len(command_str) == 0:
+            continue
+        elif command_str == "close":
+            break
+        else:
+            command = str.encode(command_str)
+            try:
+                sock.send(command)
+            except Exception as e:
+                print(f"Unable to send message to server: {e} ")
+                break
+        sleep(1)
+    return
+
+def receive_from_server():
+    while True:
+        try:
+            data = sock.recv(2048)
+        except Exception as ex:
+            print('Communication error: %s\n' % str(ex))
+            break
+        data_str = data.decode('utf-8')
+        if data == b'':
+            break
+        elif data_str == " ":
+            try:
+                sock.send(str.encode(" "))
+            except Exception as ex:
+                print(f"Unable to send msg to server: {ex}")
+                break
+        elif data_str:
+            print(data_str + "\n>>>>>", end = "")
+    return
+
+def main():
+    create_socket()
+    connect()
+    signal(SIGINT, quit_gracefully)
+    signal(SIGTERM, quit_gracefully)
+    t1 = Thread(target = receive_work, args = ())
+    t2 = Thread(target = send_work, args = ())
+    t1.daemon = True
+    t2.daemon = True
+    t1.start()
+    t2.start()
+    create_jobs()
+    sock.close()
+
+main()
---
+++ Part 3/HARD MODE Client Server/Hard Mode Server.py
@@ -0,0 +1,143 @@
+from queue import Queue
+
+from socket import AF_INET
+from socket import error
+from socket import SOCK_STREAM
+from socket import socket
+from socket import SOL_SOCKET
+from socket import SO_REUSEADDR
+
+from signal import SIGINT
+from signal import signal
+from signal import SIGTERM
+
+from sys import exit
+
+from threading import Thread
+
+from time import sleep
+
+host = ''
+port = 8888
+sock = None
+
+queue = Queue()
+temp = [1]
+
+client_connections = []
+client_addresses = []
+client_threads = []
+
+def create_socket():
+    global sock
+    try:
+        sock = socket(AF_INET, SOCK_STREAM)
+    except error as msg:
+        print("Socket creation error: " + str(msg))
+    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+def bind_socket():
+    try:
+        print("Binding the Port: " + str(port))
+        sock.bind((host, port))
+        sock.listen(5)
+    except error as msg:
+        print("Socket Binding error:" + str(msg) + "\n" + "Retrying...")
+        sleep(5)
+        bind_socket()
+
+def quit_gracefully(signal = None, frame = None):
+    for conn in client_connections:
+        try:
+            conn.shutdown(2)
+            conn.close()
+            sock.close()
+        except Exception as msg:
+            print('Could not close connection:' + str(msg))
+    exit()
+
+def accept_conn():
+    global client_connections
+    global client_addresses
+    global client_threads
+    while True:
+        try:
+            conn, addr = sock.accept()
+            client_connections.append(conn)
+            client_addresses.append(addr)
+            conn.setblocking(1)
+            t = Thread(target = send_client, args = (client_connections[-1], client_addresses[-1]))
+            t.daemon = True
+            client_threads.append(t)
+            t.start()
+        except Exception as msg:
+            print('Cannot accept connections: ' + str(msg))
+        print('Connection has been established with client: ' + str(addr))
+    return
+
+def work():
+    accept_conn()
+    queue.task_done()
+
+def create_jobs():
+    for x in temp:
+        queue.put(x)
+    queue.join()
+    return
+
+def send_client(conn, addr):
+    while True:
+        command = conn.recv(2048)
+        command_str = (command.decode('utf-8')).strip()
+        if command == b'':
+            break
+        if command_str == "connections":
+            output_str = clients()
+        elif command_str[0:4] == "send":
+            to_id = int(command_str[5])
+            msg = command_str[7:]
+            msg = f"FROM {addr}: " + msg
+            to_conn = client_connections[to_id]
+            to_addr = client_addresses[to_id]
+            try:
+                to_conn.send(str.encode(msg))
+            except Exception as ex:
+                print("Cannot send message to {} from {}: {}".format(to_addr, addr, ex))
+                continue
+            output_str = "MESSAGE sent successfully to {}\n".format(to_addr)
+        else:
+            output_str = "Please enter correct command!"
+        try:
+            conn.send(str.encode(output_str))
+        except Exception as ex:
+            print("Cannot send status message to client: {} from server:= {}".format(addr, ex))
+    return
+
+def clients():
+    clients = "CLIENTS\nCLIENT ID \tIP ADDRESS\t PORT\n"
+    for i, conn in enumerate(client_connections):
+        try:
+            conn.send(str.encode(" "))
+            data = conn.recv(2048)
+            if data == b'':
+                raise Exception("connection broken")
+        except Exception as e:
+            if i < len(client_connections):
+                del client_connections[i]
+            if i < len(client_addresses):
+                del client_addresses[i]
+            continue
+        clients += f"{i}: \t{client_addresses[i][0]}\t{client_addresses[i][1]}\n"
+    return clients
+
+def main():
+    create_socket()
+    bind_socket()
+    signal(SIGINT, quit_gracefully)
+    signal(SIGTERM, quit_gracefully)
+    t = Thread(target = work, args = ())
+    t.daemon = True
+    t.start()
+    create_jobs()
+
+main()
